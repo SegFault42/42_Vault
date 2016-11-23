d34481cb9f32b8d9358820ab2de46c2ab495e002
SegFault42 <rabougue@student.42.fr>
2016-11-23 12:28:48 +0100

    add graph algo

---
+++ wiki/algo/Lem-in/Algorithmes pour les graphes (Partie 1) _ Algorithms For Fun_files/Algorithmes pour les graphes (Partie 1) _ Algorithms For Fun_files/rate.php
@@ -0,0 +1,6 @@
+PDRTJS_2514839_post_131.avg_rating = 4.5;
+PDRTJS_2514839_post_131.votes = 3;		
+PDRTJS_settings_2514839_post_131= {			"type" : "stars",			"size" : "sml",			"star_color" : "yellow",			"custom_star" : "",			"font_size" : "",			"font_line_height" : "16px",			"font_color" : "",			"font_align" : "left",			"font_position" : "right",			"font_family" : "",			"font_bold" : "normal",			"font_italic" : "normal",			"text_vote" : "Vote",			"text_votes" : "Votes",			"text_rate_this" : "Rate This",			"text_1_star" : "Very Poor",			"text_2_star" : "Poor",			"text_3_star" : "Average",			"text_4_star" : "Good",			"text_5_star" : "Excellent",			"text_thank_you" : "Thank You",			"text_rate_up" : "Rate Up",			"text_rate_down" : "Rate Down"		};
+PDRTJS_2514839_post_131.init();		
+PDRTJS_2514839_post_131.token='16881ac2a328c9b88244272c5ca77324';
+/*2514839,_post_131,wp-post-131,1053958500,4.5-3*/
